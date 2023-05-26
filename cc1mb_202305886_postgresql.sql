0a2d878668b3041aaae880c30c174bb762f49d26
Davi Carretta Barcellos <125470461+davicarretta@users.noreply.github.com>
2023-05-25 22:48:58 -0300

    Add files via upload

--- cc1mb_202305886_postgresql.sql
+++ cc1mb_202305886_postgresql.sql
@@ -1,28 +1,31 @@
 --Excluir banco de dados caso já exista.
 DROP DATABASE IF EXISTS uvv;
 
+--Excluir schema caso já exista.
+DROP SCHEMA IF EXISTS lojas CASCADE;
+
 --Excluir usuário caso já exista.
 DROP USER IF EXISTS davicarretta;
 
---Criar usuário com permissões.
+--Criar usuário com permissões de gerenciar bancos de dados e roles.
 CREATE USER davicarretta WITH PASSWORD '5e08486a9f28ed233882abd9d4a5a8b4';
-ALTER USER davicarretta CREATEDB;
-ALTER USER davicarretta CREATEROLE;
+ALTER USER  davicarretta CREATEDB;
+ALTER USER  davicarretta CREATEROLE;
 
 --Criar banco de dados.
-CREATE DATABASE uvv
-with
-owner = davicarretta
-template = template0
-encoding = "UTF8"
-lc_collate = 'pt_BR.UTF-8'
-lc_ctype = 'pt_BR.UTF-8'
-allow_connections = true;
+CREATE DATABASE     uvv
+WITH
+OWNER             = davicarretta
+TEMPLATE          = template0
+ENCODING          = "UTF8"
+LC_COLLATE        = 'pt_BR.UTF-8'
+LC_CTYPE          = 'pt_BR.UTF-8'
+ALLOW_CONNECTIONS = true;
 
---Entrar no usuário davicarretta e ao banco de dados uvv. RESOLVEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
-\c uvv
+--Entrar no usuário davicarretta e ao banco de dados uvv.
+\connect "host=localhost port=5432 dbname=uvv user=davicarretta password=5e08486a9f28ed233882abd9d4a5a8b4 connect_timeout=1000";
 
---Criar esquema.
+--Criar esquema e permitir acesso do usuário.
 CREATE SCHEMA lojas 
 AUTHORIZATION davicarretta;
 
@@ -31,7 +34,7 @@ SET search_path TO lojas, davicarretta, public;
 ALTER USER davicarretta SET search_path TO lojas, davicarretta, public;
 
 --Criar tabela lojas.
-CREATE TABLE public.lojas (
+CREATE TABLE lojas.lojas (
                 loja_id                 NUMERIC(38)  NOT NULL ,
                 nome                    VARCHAR(255) NOT NULL ,
                 endereco_web            VARCHAR(100)          ,
@@ -45,18 +48,18 @@ CREATE TABLE public.lojas (
                 logo_ultima_atualizacao DATE                  ,
                 CONSTRAINT pk_lojas     PRIMARY KEY (loja_id)
 );
-COMMENT ON TABLE public.lojas IS 'Tabela das lojas cadastradas nas Lojas UVV.';
-COMMENT ON COLUMN public.lojas.loja_id IS 'ID''s de cada loja cadastrada.';
-COMMENT ON COLUMN public.lojas.nome IS 'Nome de cada loja cadastrada.';
-COMMENT ON COLUMN public.lojas.endereco_web IS 'Endereço Web (link on-line) para o site de cada loja cadastrada.';
-COMMENT ON COLUMN public.lojas.endereco_fisico IS 'Endereço físico de cada loja cadastrada.';
-COMMENT ON COLUMN public.lojas.latitude IS 'Latitude de cada loja cadastrada.';
-COMMENT ON COLUMN public.lojas.longitude IS 'Longitude de cada loja cadastrada.';
-COMMENT ON COLUMN public.lojas.logo IS 'Logo de cada loja cadastrada.';
-COMMENT ON COLUMN public.lojas.logo_mime_type IS 'Tipo de mídia das logos de cada loja cadastrada.';
-COMMENT ON COLUMN public.lojas.logo_arquivo IS 'Arquivo das logos de cada loja cadastrada.';
-COMMENT ON COLUMN public.lojas.logo_charset IS 'Codificação de caracteres das logos de cada loja cadastrada.';
-COMMENT ON COLUMN public.lojas.logo_ultima_atualizacao IS 'Data da última atualização das logos de cada loja cadastrada.';
+COMMENT ON TABLE lojas.lojas IS                          'Tabela das lojas cadastradas nas Lojas UVV.';
+COMMENT ON COLUMN lojas.lojas.loja_id IS                 'IDs de cada loja cadastrada.';
+COMMENT ON COLUMN lojas.lojas.nome IS                    'Nome de cada loja cadastrada.';
+COMMENT ON COLUMN lojas.lojas.endereco_web IS            'Endereço Web (link on-line) para o site de cada loja cadastrada.';
+COMMENT ON COLUMN lojas.lojas.endereco_fisico IS         'Endereço físico de cada loja cadastrada.';
+COMMENT ON COLUMN lojas.lojas.latitude IS                'Latitude de cada loja cadastrada.';
+COMMENT ON COLUMN lojas.lojas.longitude IS               'Longitude de cada loja cadastrada.';
+COMMENT ON COLUMN lojas.lojas.logo IS                    'Logo de cada loja cadastrada.';
+COMMENT ON COLUMN lojas.lojas.logo_mime_type IS          'Tipo de mídia das logos de cada loja cadastrada.';
+COMMENT ON COLUMN lojas.lojas.logo_arquivo IS            'Arquivo das logos de cada loja cadastrada.';
+COMMENT ON COLUMN lojas.lojas.logo_charset IS            'Codificação de caracteres das logos de cada loja cadastrada.';
+COMMENT ON COLUMN lojas.lojas.logo_ultima_atualizacao IS 'Data da última atualização das logos de cada loja cadastrada.';
 
 ALTER TABLE lojas
 ADD CONSTRAINT cc_lojas_loja_id
@@ -65,7 +68,8 @@ ALTER TABLE lojas
 ADD CONSTRAINT cc_lojas_nome
     CHECK (loja_id IS NOT NULL);
 
-CREATE TABLE public.produtos (
+--Criar tabela produtos.
+CREATE TABLE lojas.produtos (
                 produto_id                NUMERIC(38)   NOT NULL ,
                 nome                      VARCHAR(255)  NOT NULL ,
                 preco_unitario            NUMERIC(10,2)          ,
@@ -77,44 +81,45 @@ CREATE TABLE public.produtos (
                 imagem_ultima_atualizacao DATE                   ,
                 CONSTRAINT pk_produtos    PRIMARY KEY (produto_id)
 );
-COMMENT ON TABLE public.produtos IS 'Tabela dos produtos cadastrados nas Lojas UVV.';
-COMMENT ON COLUMN public.produtos.produto_id IS 'ID''s de cada produto cadastrado.';
-COMMENT ON COLUMN public.produtos.nome IS 'Nome de cada produto cadastrado.';
-COMMENT ON COLUMN public.produtos.preco_unitario IS 'Preço unitário de cada produto cadastrado.';
-COMMENT ON COLUMN public.produtos.detalhes IS 'Detalhes de cada produto cadastrado.';
-COMMENT ON COLUMN public.produtos.imagem IS 'Imagem de cada produto cadastrado.';
-COMMENT ON COLUMN public.produtos.imagem_mime_type IS 'Tipo de mídia das imagens de cada produto cadastrado.';
-COMMENT ON COLUMN public.produtos.imagem_arquivo IS 'Arquivo das imagens de cada produto cadastrado.';
-COMMENT ON COLUMN public.produtos.imagem_charset IS 'Codificação de caracteres das imagens de cada produto cadastrado.';
-COMMENT ON COLUMN public.produtos.imagem_ultima_atualizacao IS 'Data da última atualização das imagens de cada produto cadastrado.';
+COMMENT ON TABLE lojas.produtos IS                            'Tabela dos produtos cadastrados nas Lojas UVV.';
+COMMENT ON COLUMN lojas.produtos.produto_id IS                'IDs de cada produto cadastrado.';
+COMMENT ON COLUMN lojas.produtos.nome IS                      'Nome de cada produto cadastrado.';
+COMMENT ON COLUMN lojas.produtos.preco_unitario IS            'Preço unitário de cada produto cadastrado.';
+COMMENT ON COLUMN lojas.produtos.detalhes IS                  'Detalhes de cada produto cadastrado.';
+COMMENT ON COLUMN lojas.produtos.imagem IS                    'Imagem de cada produto cadastrado.';
+COMMENT ON COLUMN lojas.produtos.imagem_mime_type IS          'Tipo de mídia das imagens de cada produto cadastrado.';
+COMMENT ON COLUMN lojas.produtos.imagem_arquivo IS            'Arquivo das imagens de cada produto cadastrado.';
+COMMENT ON COLUMN lojas.produtos.imagem_charset IS            'Codificação de caracteres das imagens de cada produto cadastrado.';
+COMMENT ON COLUMN lojas.produtos.imagem_ultima_atualizacao IS 'Data da última atualização das imagens de cada produto cadastrado.';
 
-
-CREATE TABLE public.estoques (
+--Criar tabela estoques.
+CREATE TABLE lojas.estoques (
                 estoque_id NUMERIC(38) NOT NULL ,
                 loja_id    NUMERIC(38) NOT NULL ,
                 produto_id NUMERIC(38) NOT NULL ,
                 quantidade NUMERIC(38) NOT NULL ,
                 CONSTRAINT pk_estoques PRIMARY KEY (estoque_id)
 );
-COMMENT ON TABLE public.estoques IS 'Tabela dos estoques cadastrados nas Lojas UVV.';
-COMMENT ON COLUMN public.estoques.loja_id IS 'ID''s de cada loja cadastrada.';
-COMMENT ON COLUMN public.estoques.produto_id IS 'ID''s de cada produto cadastrado.';
-COMMENT ON COLUMN public.estoques.quantidade IS 'Quantidade de estoque de cada produto em cada loja cadastrada.';
+COMMENT ON TABLE lojas.estoques IS             'Tabela dos estoques cadastrados nas Lojas UVV.';
+COMMENT ON COLUMN lojas.estoques.loja_id IS    'IDs de cada loja cadastrada.';
+COMMENT ON COLUMN lojas.estoques.produto_id IS 'IDs de cada produto cadastrado.';
+COMMENT ON COLUMN lojas.estoques.quantidade IS 'Quantidade de estoque de cada produto em cada loja cadastrada.';
 
 ALTER TABLE estoques 
 ADD CONSTRAINT cc_estoques_estoque_id
-    CHECK (estoque_id between 1 AND 38);
+    CHECK (estoque_id > 0 AND < 100000000000000000000000000000000000000);
 ALTER TABLE estoques 
 ADD CONSTRAINT cc_estoques_loja_id
-    CHECK (loja_id between 1 AND 38);
+    CHECK (loja_id > 0 AND < 100000000000000000000000000000000000000);
 ALTER TABLE estoques
 ADD CONSTRAINT cc_estoques_produto_id
-    CHECK (produto_id between 1 AND 38);
+    CHECK (produto_id > 0 AND < 100000000000000000000000000000000000000);
 ALTER TABLE estoques
 ADD CONSTRAINT cc_estoques_quantidade
-    CHECK (quantidade between 1 AND 38);
+    CHECK (quantidade >= 0 AND ROUND(quantidade));
 
-CREATE TABLE public.clientes (
+--Criar tabela clientes.
+CREATE TABLE lojas.clientes (
                 cliente_id NUMERIC(38)  NOT NULL ,
                 email      VARCHAR(255) NOT NULL ,
                 nome       VARCHAR(255) NOT NULL ,
@@ -123,17 +128,17 @@ CREATE TABLE public.clientes (
                 telefone3  VARCHAR(20)           ,
                 CONSTRAINT pk_clientes PRIMARY KEY (cliente_id)
 );
-COMMENT ON TABLE public.clientes IS 'Tabela dos clientes cadastrados nas Lojas UVV.';
-COMMENT ON COLUMN public.clientes.cliente_id IS 'ID''s de cada cliente cadastrado.';
-COMMENT ON COLUMN public.clientes.email IS 'Email de cada cliente cadastrado.';
-COMMENT ON COLUMN public.clientes.nome IS 'Nome de cada cliente cadastrado.';
-COMMENT ON COLUMN public.clientes.telefone1 IS 'Telefone 1 de cada cliente cadastrado.';
-COMMENT ON COLUMN public.clientes.telefone2 IS 'Telefone 2 de cada cliente cadastrado.';
-COMMENT ON COLUMN public.clientes.telefone3 IS 'Telefone 3 de cada cliente cadastrado.';
+COMMENT ON TABLE lojas.clientes IS             'Tabela dos clientes cadastrados nas Lojas UVV.';
+COMMENT ON COLUMN lojas.clientes.cliente_id IS 'IDs de cada cliente cadastrado.';
+COMMENT ON COLUMN lojas.clientes.email IS      'Email de cada cliente cadastrado.';
+COMMENT ON COLUMN lojas.clientes.nome IS       'Nome de cada cliente cadastrado.';
+COMMENT ON COLUMN lojas.clientes.telefone1 IS  'Telefone 1 de cada cliente cadastrado.';
+COMMENT ON COLUMN lojas.clientes.telefone2 IS  'Telefone 2 de cada cliente cadastrado.';
+COMMENT ON COLUMN lojas.clientes.telefone3 IS  'Telefone 3 de cada cliente cadastrado.';
 
 ALTER TABLE clientes
 ADD CONSTRAINT cc_clientes_cliente_id
-    CHECK (cliente_id between 1 AND 38);
+    CHECK (cliente_id BETWEEN > 0 AND < 100000000000000000000000000000000000000);
 ALTER TABLE clientes
 ADD CONSTRAINT cc_clientes_email
     CHECK (email IS NOT NULL);
@@ -141,7 +146,8 @@ ALTER TABLE clientes
 ADD CONSTRAINT cc_clientes_nome
     CHECK (email IS NOT NULL);
 
-CREATE TABLE public.pedidos (
+--Criar tabela pedidos.
+CREATE TABLE lojas.pedidos (
                 pedido_id  NUMERIC  (38) NOT NULL ,
                 data_hora  TIMESTAMP     NOT NULL ,
                 cliente_id NUMERIC  (38) NOT NULL ,
@@ -149,15 +155,31 @@ CREATE TABLE public.pedidos (
                 loja_id    NUMERIC  (38) NOT NULL ,
                 CONSTRAINT pk_pedidos PRIMARY KEY (pedido_id)
 );
-COMMENT ON TABLE public.pedidos IS 'Tabela dos pedidos cadastrados nas Lojas UVV.';
-COMMENT ON COLUMN public.pedidos.pedido_id IS 'ID''s de cada pedido cadastrado.';
-COMMENT ON COLUMN public.pedidos.data_hora IS 'Data e hora de cada pedido cadastrado.';
-COMMENT ON COLUMN public.pedidos.cliente_id IS 'ID''s de cada cliente cadastrado.';
-COMMENT ON COLUMN public.pedidos.status IS 'Status de cada pedido cadastrado.';
-COMMENT ON COLUMN public.pedidos.loja_id IS 'ID''s de cada loja cadastrada.';
+COMMENT ON TABLE lojas.pedidos IS             'Tabela dos pedidos cadastrados nas Lojas UVV.';
+COMMENT ON COLUMN lojas.pedidos.pedido_id IS  'IDs de cada pedido cadastrado.';
+COMMENT ON COLUMN lojas.pedidos.data_hora IS  'Data e hora de cada pedido cadastrado.';
+COMMENT ON COLUMN lojas.pedidos.cliente_id IS 'IDs de cada cliente cadastrado.';
+COMMENT ON COLUMN lojas.pedidos.status IS     'Status de cada pedido cadastrado.';
+COMMENT ON COLUMN lojas.pedidos.loja_id IS    'IDs de cada loja cadastrada.';
 
+ALTER TABLE pedidos
+ADD CONSTRAINT cc_pedidos_pedidos_id
+    CHECK (pedido_id IS NOT NULL AND pedido_id > 0);
+ALTER TABLE pedidos
+ADD CONSTRAINT cc_pedidos_data_hora
+    CHECK (data_hora IS NOT NULL AND data_hora > 1672531200);
+ALTER TABLE pedidos
+ADD CONSTRAINT cc_pedidos_cliente_id
+    CHECK (cliente_id IS NOT NULL AND cliente_id > 0);
+ALTER TABLE pedidos
+ADD CONSTRAINT cc_pedidos_status
+    CHECK (status IS NOT NULL AND status IN ('CANCELADO', 'COMPLETO', 'ABERTO', 'PAGO', 'REEMBOLSADO', 'ENVIADO'));
+ALTER TABLE pedidos
+ADD CONSTRAINT cc_pedidos_loja_id
+    CHECK (loja_id IS NOT NULL AND loja_id > 0);
 
-CREATE TABLE public.envios (
+--Criar tabela envios.
+CREATE TABLE lojas.envios (
                 envio_id         NUMERIC(38)  NOT NULL ,
                 loja_id          NUMERIC(38)  NOT NULL ,
                 cliente_id       NUMERIC(38)  NOT NULL ,
@@ -165,15 +187,15 @@ CREATE TABLE public.envios (
                 status           VARCHAR(15)  NOT NULL ,
                 CONSTRAINT pk_envios PRIMARY KEY (envio_id)
 );
-COMMENT ON TABLE public.envios IS 'Tabela dos envios cadastrados nas Lojas UVV.';
-COMMENT ON COLUMN public.envios.envio_id IS 'ID''s de cada envio cadastrado.';
-COMMENT ON COLUMN public.envios.loja_id IS 'ID''s de cada loja cadastrada.';
-COMMENT ON COLUMN public.envios.cliente_id IS 'ID''s de cada cliente cadastrado.';
-COMMENT ON COLUMN public.envios.endereco_entrega IS 'Endereço de entrega de cada envio cadastrado.';
-COMMENT ON COLUMN public.envios.status IS 'Status de cada envio cadastrado.';
+COMMENT ON TABLE lojas.envios IS                   'Tabela dos envios cadastrados nas Lojas UVV.';
+COMMENT ON COLUMN lojas.envios.envio_id IS         'IDs de cada envio cadastrado.';
+COMMENT ON COLUMN lojas.envios.loja_id IS          'IDs de cada loja cadastrada.';
+COMMENT ON COLUMN lojas.envios.cliente_id IS       'IDs de cada cliente cadastrado.';
+COMMENT ON COLUMN lojas.envios.endereco_entrega IS 'Endereço de entrega de cada envio cadastrado.';
+COMMENT ON COLUMN lojas.envios.status IS           'Status de cada envio cadastrado.';
 
-
-CREATE TABLE public.pedidos_itens (
+--Criar tabelas pedidos_itens.
+CREATE TABLE lojas.pedidos_itens (
                 pedido_id       NUMERIC(38)   NOT NULL ,
                 produto_id      NUMERIC(38)   NOT NULL ,
                 numero_da_linha NUMERIC(38)   NOT NULL ,
@@ -182,73 +204,72 @@ CREATE TABLE public.pedidos_itens (
                 envio_id        NUMERIC(38)            ,
                 CONSTRAINT pk_pedidos_itens PRIMARY KEY (pedido_id, produto_id)
 );
-COMMENT ON COLUMN public.pedidos_itens.pedido_id IS 'ID''s de cada pedido cadastrado.';
-COMMENT ON COLUMN public.pedidos_itens.produto_id IS 'ID''s de cada produto cadastrado.';
-COMMENT ON COLUMN public.pedidos_itens.numero_da_linha IS 'Número da linha de cada pedido de itens cadastrado.';
-COMMENT ON COLUMN public.pedidos_itens.preco_unitario IS 'Preço unitário de cada produto cadastrado.';
-COMMENT ON COLUMN public.pedidos_itens.quantidade IS 'Quantidade de pedidos de cada item cadastrado.';
-COMMENT ON COLUMN public.pedidos_itens.envio_id IS 'ID''s de cada envio cadastrado.';
+COMMENT ON COLUMN lojas.pedidos_itens.pedido_id IS       'IDs de cada pedido cadastrado.';
+COMMENT ON COLUMN lojas.pedidos_itens.produto_id IS      'IDs de cada produto cadastrado.';
+COMMENT ON COLUMN lojas.pedidos_itens.numero_da_linha IS 'Número da linha de cada pedido de itens cadastrado.';
+COMMENT ON COLUMN lojas.pedidos_itens.preco_unitario IS  'Preço unitário de cada produto cadastrado.';
+COMMENT ON COLUMN lojas.pedidos_itens.quantidade IS      'Quantidade de pedidos de cada item cadastrado.';
+COMMENT ON COLUMN lojas.pedidos_itens.envio_id IS        'IDs de cada envio cadastrado.';
 
-
-ALTER TABLE public.envios ADD CONSTRAINT lojas_envios_fk
+ALTER TABLE lojas.envios ADD CONSTRAINT lojas_envios_fk
 FOREIGN KEY (loja_id)
-REFERENCES public.lojas (loja_id)
+REFERENCES lojas.lojas (loja_id)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
-ALTER TABLE public.pedidos ADD CONSTRAINT lojas_pedidos_fk
+ALTER TABLE lojas.pedidos ADD CONSTRAINT lojas_pedidos_fk
 FOREIGN KEY (loja_id)
-REFERENCES public.lojas (loja_id)
+REFERENCES lojas.lojas (loja_id)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
-ALTER TABLE public.estoques ADD CONSTRAINT lojas_estoques_fk
+ALTER TABLE lojas.estoques ADD CONSTRAINT lojas_estoques_fk
 FOREIGN KEY (loja_id)
-REFERENCES public.lojas (loja_id)
+REFERENCES lojas.lojas (loja_id)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
-ALTER TABLE public.pedidos_itens ADD CONSTRAINT produtos_pedidos_itens_fk
+ALTER TABLE lojas.pedidos_itens ADD CONSTRAINT produtos_pedidos_itens_fk
 FOREIGN KEY (produto_id)
-REFERENCES public.produtos (produto_id)
+REFERENCES lojas.produtos (produto_id)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
-ALTER TABLE public.estoques ADD CONSTRAINT produtos_estoques_fk
+ALTER TABLE lojas.estoques ADD CONSTRAINT produtos_estoques_fk
 FOREIGN KEY (produto_id)
-REFERENCES public.produtos (produto_id)
+REFERENCES lojas.produtos (produto_id)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
-ALTER TABLE public.envios ADD CONSTRAINT clientes_envios_fk
+ALTER TABLE lojas.envios ADD CONSTRAINT clientes_envios_fk
 FOREIGN KEY (cliente_id)
-REFERENCES public.clientes (cliente_id)
+REFERENCES lojas.clientes (cliente_id)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
-ALTER TABLE public.pedidos ADD CONSTRAINT clientes_pedidos_fk
+ALTER TABLE lojas.pedidos ADD CONSTRAINT clientes_pedidos_fk
 FOREIGN KEY (cliente_id)
-REFERENCES public.clientes (cliente_id)
+REFERENCES lojas.clientes (cliente_id)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
-ALTER TABLE public.pedidos_itens ADD CONSTRAINT pedidos_pedidos_itens_fk
+ALTER TABLE lojas.pedidos_itens ADD CONSTRAINT pedidos_pedidos_itens_fk
 FOREIGN KEY (pedido_id)
-REFERENCES public.pedidos (pedido_id)
+REFERENCES lojas.pedidos (pedido_id)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
-ALTER TABLE public.pedidos_itens ADD CONSTRAINT envios_pedidos_itens_fk
+ALTER TABLE lojas.pedidos_itens ADD CONSTRAINT envios_pedidos_itens_fk
 FOREIGN KEY (envio_id)
-REFERENCES public.envios (envio_id)
+REFERENCES lojas.envios (envio_id)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
